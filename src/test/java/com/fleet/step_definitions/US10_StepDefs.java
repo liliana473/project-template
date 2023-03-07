0a97f0200421e2c013a06634da17f9619e63d76b
atakav <114355154+atakav@users.noreply.github.com>
2023-03-06 20:58:17 -0500

    US10_Ata

---
+++ src/test/java/com/fleet/step_definitions/US10_StepDefs.java
@@ -0,0 +1,27 @@
+package com.fleet.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US10_StepDefs {
+    @Given("the user click Click the “Calendar Events” under the Activities")
+    public void the_user_click_click_the_calendar_events_under_the_activities() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("Click the “Create Calendar Event” button")
+    public void click_the_create_calendar_event_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("Write a message in the Description field")
+    public void write_a_message_in_the_description_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user Verify the message is written in the input box")
+    public void the_user_verify_the_message_is_written_in_the_input_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
