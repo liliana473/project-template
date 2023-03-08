9e0cff62b59b4afa270675028103ffcb1c0df3d0
avfil <avfilatova143@gmail.com>
2023-03-08 17:28:33 -0500

    Sasha`s final commit edited

--- src/test/java/com/fleet/pages/VehicleColumnPage.java
+++ src/test/java/com/fleet/pages/VehicleColumnPage.java
@@ -1,7 +1,18 @@
 package com.fleet.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class VehicleColumnPage {
+import java.util.List;
+
+public class VehicleColumnPage extends BasePage {
+
+    @FindBy(css = ".grid-header-cell__label")
+    public List<WebElement> columnOptions;
+
 
 }
+
+
+
--- src/test/java/com/fleet/step_definitions/US5_StepDefs.java
+++ src/test/java/com/fleet/step_definitions/US5_StepDefs.java
@@ -1,28 +1,31 @@
 package com.fleet.step_definitions;
 
 import com.fleet.pages.DashboardPage;
+import com.fleet.pages.VehicleColumnPage;
+import com.fleet.utilities.BrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class US5_StepDefs {
 
     DashboardPage dashboardPage = new DashboardPage();
-    @Given("the user navigate to {string}-{string}")
-    public void the_user_navigate_to(String string, String string2) {
+
+
+    @Then("the user should see below options")
+    public void theUserShouldSeeBelowOptions(List<String> expectedOptions) {
+        List<String> actualOptions = BrowserUtils.getElementsText(new VehicleColumnPage().columnOptions);
+
+        Assert.assertEquals(actualOptions, expectedOptions);
 
     }
-    @Then("the user should see below {string}")
-    public void the_user_should_see_below(String string) {
-
-    }
-
-
-
-
-
-    }
+}
 
 
 
